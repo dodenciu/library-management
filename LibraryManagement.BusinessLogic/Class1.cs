70ffc2b7cdb89c9cde855459fbc48d63ede2e5e7
Vlad Daniel Dodenciu <dodenciu@tutanota.com>
2020-07-18 21:44:39 +0300

    api project added"

--- LibraryManagement.BusinessLogic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LibraryManagement.BusinessLogic
-{
-    public class Class1
-    {
-    }
-}
---
+++ LibraryManagement.BusinessLogic/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.BusinessLogic
+{
+    //public class Person
+    //{
+    //    private String name;
+    //    private Address address;
+    //    private String email;
+    //    private String phone;
+    //}
+    public class Person
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
